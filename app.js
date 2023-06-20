5af8a08a3ac1908a522376539673adb4106717f8
Arturo Espinosa <pupitetris@yahoo.com>
2023-06-19 20:39:46 -0600

    Nodejs + Express mini-server, importing official cesium npm.

---
+++ app.js
@@ -0,0 +1,16 @@
+const Express = require('express');
+const Morgan = require('morgan');
+
+const PORT = 8001;
+
+const app = Express();
+
+app.use(Morgan('common'));
+app.use(Express.urlencoded({ extended: true }));
+
+app.use('/', Express.static(__dirname + '/htdocs'));
+app.use('/src', Express.static(__dirname + '/src'));
+app.use('/cesium', Express.static(__dirname + '/node_modules/cesium/Build/CesiumUnminified'));
+app.get('/', (req, res) => { res.redirect('index.html'); });
+
+app.listen(PORT);
