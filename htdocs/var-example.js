89c5212d9fb3b276caa21aa0b55379b3d8464fe3
Arturo Espinosa <pupitetris@yahoo.com>
2023-06-24 19:55:33 -0600

    htdocs/var-example.js: An example. Just add your tokens.

---
+++ htdocs/var-example.js
@@ -0,0 +1,11 @@
+const VAR = {
+    bingKey: "get-yours-at-https://www.bingmapsportal.com/",
+    cesiumKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJmYmE2YWEzOS1lZDUyLTQ0YWMtOTlkNS0wN2VhZWI3NTc4MmEiLCJpZCI6MjU5LCJpYXQiOjE2ODU2MzQ0Njl9.AswCMxsN03WYwuZL-r183OZicN64Ks9aPExWhA3fuLY"
+};
+
+Object.keys(VAR).forEach(key => {
+    Object.defineProperty(VAR, key, { value: VAR[key], enumerable: true, writable: false, configurable: false });
+});
+
+export default VAR;
+
