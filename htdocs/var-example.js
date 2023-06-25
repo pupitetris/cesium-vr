c261a0cc3716c3aa003108b462f447522e9c81a7
Arturo Espinosa <pupitetris@yahoo.com>
2023-06-24 19:56:06 -0600

    htdocs/var-example.js: added comment on the Cesium token.

--- htdocs/var-example.js
+++ htdocs/var-example.js
@@ -1,5 +1,6 @@
 const VAR = {
     bingKey: "get-yours-at-https://www.bingmapsportal.com/",
+    // Globally default CesiumIon key, will generate a warning:
     cesiumKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJmYmE2YWEzOS1lZDUyLTQ0YWMtOTlkNS0wN2VhZWI3NTc4MmEiLCJpZCI6MjU5LCJpYXQiOjE2ODU2MzQ0Njl9.AswCMxsN03WYwuZL-r183OZicN64Ks9aPExWhA3fuLY"
 };
 
