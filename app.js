63201d6084520d9fc98fd8d3601e8fc5f8b383d2
Arturo Espinosa <pupitetris@yahoo.com>
2023-07-26 10:10:20 -0600

    app.js: report listening port when starting the http server.

--- app.js
+++ app.js
@@ -13,4 +13,4 @@ app.use("/src", Express.static(__dirname + "/src"));
 app.use("/cesium", Express.static(__dirname + "/node_modules/cesium/Build/CesiumUnminified"));
 app.get("/", (req, res) => { res.redirect("index.html"); });
 
-app.listen(PORT);
+app.listen(PORT, () => { console.log('listening on port ' + PORT)});
